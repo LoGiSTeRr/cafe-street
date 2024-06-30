76146cc052659bf9f49b8c1e75e41c731495eaea
LoGiSTeRr <danikbor160@Gmail.com>
2024-06-30 15:23:50 +0400

    Add Header component to HomePage

--- app/HomePage/page.jsx
+++ app/HomePage/page.jsx
@@ -3,33 +3,35 @@ import Image from 'next/image';
 import Layout from "@/app/Layout/Layout";
 import {IoCartOutline} from "react-icons/io5";
 import {FaStar} from "react-icons/fa";
+import Header from "@/app/Layout/Header";
 
 const HomePage = () => {
     return (
         <Layout>
             <div className="flex flex-col items-center gap-5 overflow-x-hidden">
-                <section className="bg-[#F6EBDA] w-full h-full flex items-center justify-between px-24 py-40">
-                    <div className="flex flex-col gap-5">
-                        <div className="text-secondary font-semibold text-[48px]">
-                            <p>
-                                Enjoy your <span className="text-primary">coffee</span>
-                            </p>
-                            <p>before your activity</p>
-                        </div>
-                        <div className="text-light text-[18px]">
-                            <p>Boost your productivity and build your </p>
-                            <p>mood with a glass of coffee in the morning </p>
-                        </div>
-                        <div className="flex flex-row items-center gap-10">
-                            <div
-                                className="bg-secondary rounded-[20px] h-auto px-10 py-3 flex flex-row items-center justify-center gap-3">
-                                <p className="font-bold text-[14px]">Order now</p>
-                                <IoCartOutline className="bg-primary rounded-full text-[28px] p-1"/>
+                <section className="bg-[#F6EBDA] w-full h-full flex flex-col items-center justify-between px-24 py-10 gap-10">
+                    <Header/>
+                    <div className="bg-[#F6EBDA] w-full h-full flex items-center justify-between">
+                        <div className="flex flex-col gap-5">
+                            <div className="text-secondary font-semibold text-[48px]">
+                                <p>
+                                    Enjoy your <span className="text-primary">coffee</span>
+                                </p>
+                                <p>before your activity</p>
                             </div>
-                            <p className="text-primary text-[12px] font-bold">More Menu</p>
+                            <div className="text-light text-[18px]">
+                                <p>Boost your productivity and build your </p>
+                                <p>mood with a glass of coffee in the morning </p>
+                            </div>
+                            <div className="flex flex-row items-center gap-10">
+                                <div
+                                    className="bg-secondary rounded-[20px] h-auto px-10 py-3 flex flex-row items-center justify-center gap-3">
+                                    <p className="font-bold text-[14px]">Order now</p>
+                                    <IoCartOutline className="bg-primary rounded-full text-[28px] p-1"/>
+                                </div>
+                                <p className="text-primary text-[12px] font-bold">More Menu</p>
+                            </div>
                         </div>
-                    </div>
-                    <div>
                         <div className="relative bg-secondary rounded-full">
                             <Image alt="coffee" src="/hero-coffee.png" width={1000} height={1000}
                                    className="p-12 w-auto h-[400px]"/>
---
+++ app/Layout/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Image from 'next/image'
+import Link from "next/link";
+import {BiSearch} from "react-icons/bi";
+import {IoCartOutline} from "react-icons/io5";
+
+const Header = () => {
+    return (
+        <div className="w-full flex flex-row items-center justify-between z-[100]">
+            <Link href="/">
+                <Image src="/logo.png" width={1100} height={234} alt="logo" className="w-auto h-[30px]"/>
+            </Link>
+            <div className="text-secondary flex flex-row gap-5 items-center justify-center">
+                <Link href="/" className="hover:text-primary">About Us</Link>
+                <Link href="/" className="hover:text-primary">Our Product</Link>
+                <Link href="/" className="hover:text-primary">Delivery</Link>
+            </div>
+            <div className="flex items-center gap-8">
+                <div className="relative ">
+                    <input type="text" className="rounded-full border border-gray-300 pl-10 px-4 py-3 text-secondary"
+                           placeholder="Cappucino"/>
+                    <BiSearch className="h-6 w-6 text-secondary absolute top-3.5 left-3"/>
+                </div>
+                <div className="relative">
+                    <IoCartOutline className="text-[28px] text-secondary h-10 w-10"/>
+                    <div className="absolute top-0 left-7 rounded-full w-4 h-4 border-white border-2 bg-primary"/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
