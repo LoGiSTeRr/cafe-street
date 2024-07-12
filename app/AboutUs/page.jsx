c8b53c497b7d54ee3724dfd2ef0e4ceeb5f7f070
LoGiSTeRr <danikbor160@Gmail.com>
2024-07-12 10:58:24 +0400

    about us completed

--- app/AboutUs/page.jsx
+++ app/AboutUs/page.jsx
@@ -14,20 +14,20 @@ const AboutUs = () => {
             <div className="flex flex-col items-center bg-[#F6EBDA] overflow-x-hidden">
                 <section
                     className="bg-[#F6EBDA] w-full h-full flex flex-col lg:flex-row items-center justify-between px-8 py-10 lg:py-12 pt-5 gap-16 lg:gap-20 max-w-7xl mx-auto">
-                    <div className="w-[45%] bg-white bg-opacity-65 p-2.5 rounded-2xl">
+                    <div className="lg:w-[45%] w-[70%] bg-white bg-opacity-65 p-2.5 rounded-2xl">
                         <Image alt="coffee" src="/aboutus-bg.jpg" width={512} height={512}
                                className="w-full h-auto rounded-2xl"/>
                     </div>
                     <div
-                        className="lg:w-[50%] flex flex-col gap-5 items-center justify-center lg:text-left lg:items-start">
-                        <p className="text-3xl lg:text-6xl text-secondary font-semibold items-center justify-center lg:text-left lg:items-start text-center">
+                        className="lg:w-[50%] w-full flex flex-col gap-5 items-center justify-center lg:text-left lg:items-start">
+                        <h1 className="text-3xl lg:text-6xl text-secondary font-semibold items-center justify-center lg:text-left lg:items-start text-center">
                             About us
-                        </p>
-                        <p className="text-light text-sm lg:text-lg items-center justify-center lg:text-left lg:items-start text-center">
+                        </h1>
+                        <p className="text-light text-sm lg:text-lg items-center justify-center lg:text-left lg:items-start text-center lg:max-w-7xl max-w-2xl mx-auto">
                             We are a dedicated team of professionals with a passion for what we do. With a
                             commitment to quality and authenticity, our goal is to provide our customers with
                             an unparalleled experience. </p>
-                        <p className="text-light text-sm lg:text-lg items-center justify-center lg:text-left lg:items-start text-center">
+                        <p className="text-light text-sm lg:text-lg items-center justify-center lg:text-left lg:items-start text-center lg:max-w-7xl max-w-2xl mx-auto">
                             it's creating a unique product, offering exceptional
                             service, or standing behind our values, we strive to exceed expectations in everything we
                             do. </p>
@@ -40,31 +40,31 @@ const AboutUs = () => {
                     </div>
                 </section>
                 <section
-                    className="bg-[#F6EBDA] w-full h-full flex flex-wrap lg:flex-row items-center justify-center py-10 lg:py-12">
+                    className="bg-[#F6EBDA] w-full h-full flex flex-wrap lg:flex-row items-center justify-center">
                     <div style={{
                         backgroundImage: `url(/cozy.jpg)`,
                         backgroundSize: `cover`,
                         backgroundPosition: `center`
                     }} className="w-full h-full">
-                        <div className="w-full h-full bg-black bg-opacity-65 flex flex-row ">
+                        <div className="w-full h-full bg-black bg-opacity-65">
                             <div
-                                className="px-8 py-14 lg:py-20 pt-5 max-w-7xl mx-auto flex flex-row items-center justify-center">
+                                className="px-8 py-14 lg:py-20 pt-5 max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-center gap-12 lg:gap-0">
                                 <div
-                                    className="flex flex-col gap-2 justify-center items-center text-center border-r-primary border-r-[1px] px-8">
+                                    className="flex flex-col gap-2 justify-center items-center text-center lg:border-r-primary lg:border-r-[1px] lg:px-8">
                                     <FaCoffee className="text-[50px] text-primary"/> {/*Icon change*/}
                                     <h2 className="text-[20px]">Specialty Coffee</h2> {/*Text & tag change*/}
                                     <p className="text-[16px]">Enjoy our wide variety of beans roasted
                                         in-house.</p> {/*Text change*/}
                                 </div>
                                 <div
-                                    className="flex flex-col gap-2 justify-center items-center text-center border-r-primary border-r-[1px] px-8">
+                                    className="flex flex-col gap-2 justify-center items-center text-center lg:border-r-primary lg:border-r-[1px] lg:px-8">
                                     <GiCroissant className="text-[50px] text-primary"/> {/*Icon change*/}
                                     <h2 className="text-[20px]">Fresh Pastries</h2> {/*Text & tag change*/}
                                     <p className="text-[16px]">Baked daily for the best breakfast
                                         experience.</p> {/*Text change*/}
                                 </div>
                                 <div
-                                    className="flex flex-col gap-2 justify-center items-center text-center border-r-primary border-r-[1px] px-8">
+                                    className="flex flex-col gap-2 justify-center items-center text-center lg:border-r-primary lg:border-r-[1px] lg:px-8">
                                     <IoFastFoodOutline className="text-[50px] text-primary"/> {/*Icon change*/}
                                     <h2 className="text-[20px]">Delicious Meals</h2> {/*Text & tag change*/}
                                     <p className="text-[16px]">Healthy and flavorful dishes prepared
@@ -78,6 +78,32 @@ const AboutUs = () => {
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </section>
+                <section
+                    className="bg-[#F6EBDA] w-full h-full">
+                    <div
+                        className="flex flex-col items-center justify-center gap-5 px-8 py-16 lg:py-24 pt-5 max-w-7xl mx-auto">
+                        <h2 className="text-4xl lg:text-6xl text-secondary font-semibold items-center justify-center lg:text-left lg:items-start text-center">
+                            Our Story
+                        </h2>
+                        <div className="flex flex-row gap-5 items-center justify-center">
+                            <Image src="/dashed-line.png" alt="line" width={300} height={60}
+                                   className="w-auto h-[35px]"/>
+                            <Image src="/coffee-beans.png" alt="coffee bean" width={500} height={500}
+                                   className="w-auto h-[35px]"/>
+                            <Image src="/dashed-line.png" alt="line" width={300} height={60} className="w-auto h-[35px]"
+                                   style={{transform: "scaleX(-1)"}}/>
+                        </div>
+                        <p className="text-secondary text-sm lg:text-xl items-center justify-center lg:items-start text-center max-w-4xl mx-auto" style={{lineHeight: "2"}}>
+                            Our story began in the vibrant city of Portland, Oregon. It was there, in that city full of
+                            coffee-enthusiasts, that we discovered the true power and potential of a humble coffee bean.
+                            From the moment we served our first hand-crafted brew, we knew we were onto something
+                            special. Here at Portland Brew, we celebrate the beauty of simplicity and the craft of true
+                            artisan roasting. Every batch of beans is roasted with love and precision, transforming them
+                            into the rich and full-bodied coffee you enjoy today. Our mission is not just to serve
+                            excellent coffee, but to spark joy and to elevate your day, one cup at a time.
+                        </p>
                     </div>
                 </section>
             </div>
